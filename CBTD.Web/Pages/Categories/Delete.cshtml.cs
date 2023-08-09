661099ce3d9320a322bc72bb18a030bc65996ea5
Chi Nguyen <chi.nguyen@live.com>
2023-08-10 01:25:37 +0300

    add delete page and function for category

---
+++ CBTD.Web/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+using CBTD.DataAccess.Data;
+using CBTD.Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CBTD.Web.Pages.Categories
+{
+    public class Delete : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public  Category ObjCategory { get; set; }
+
+ 
+        public Delete(ApplicationDbContext db)
+        {
+            _db = db;
+            ObjCategory = new ();
+        }
+
+        public IActionResult OnGet(int? id)
+        {
+            ObjCategory = new Category();
+            if(id != 0)
+            {
+                ObjCategory = _db.Categories.Find(id);
+            }
+            if(ObjCategory == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //if this is a new category
+            if (ObjCategory.Id == 0)
+            {
+                _db.Categories.Add(ObjCategory);
+                TempData["success"] = "Category added Successfully";
+            }
+            //if category exists
+            else
+            {
+                _db.Categories.Remove(ObjCategory);
+                TempData["success"] = "Category deleted Successfully";
+            }
+                _db.SaveChanges();
+
+            return RedirectToPage("./Index");
+	    }
+    }
+}
